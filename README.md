86f70ef1a8cc0e3a9609f9d4b2513be30aec2d3b
Sankalp Jha <jha.sankalp@gmail.com>
2018-10-11 00:47:41 +0530

    Application Config and README changes

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# SpringBootRestAPI
+[2~# SpringBootRestAPI Project
+
+This is a basic spring boot web project required for beginners.  
+This spring-boot application is basic implementation of what you can do with SPRING.
+
+## Prerequsites
+
+Prior having a look at this project one needs to have basic knowledge of spring, RESTful API's, java, hibernate and POJO.
+
+### Features 
+
+This project has implementation of below features:
+* Spring-boot
+* RESTful API
+* H2-JPA Implementation
+* SSL Implementation
+* Basic Authentication or Login Machenism
+
+### Installation
+
+Clone this project in directory of your mechanism.  
+Import it as MAVEN project.
--- src/main/java/com/example/ews/configuration/ApplicationConfig.java
+++ src/main/java/com/example/ews/configuration/ApplicationConfig.java
@@ -1,10 +1,14 @@
 package com.example.ews.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@EnableWebSecurity 
+@Configuration 
 public class ApplicationConfig extends WebSecurityConfigurerAdapter{
 
 	@Autowired
